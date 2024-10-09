594d2a1b92c14cc2636df26ec089f9abc4a49a37
Sandeep <sandeepmodiwork@gmail.comm>
2024-10-09 21:26:35 +0530

    CAL-04-Sum for values with Enter pressed

--- calculator.js
+++ calculator.js
@@ -3,6 +3,11 @@ const calculator = (nums) => {
     return 0;
   }
 
+  if (nums.includes("\n")) {
+    let numbers = nums.replaceAll("\n", ",");
+    nums = numbers;
+  }
+
   let total = 0;
 
   const list = nums.split(",").map((el) => Math.round(el));
@@ -19,5 +24,6 @@ console.log(calculator("2,3,4,5"));
 console.log(calculator("2,3.3,4.1,5.2"));
 console.log(calculator("2,3,4.6,5.8"));
 console.log(calculator("2,3,4,5    "));
+console.log(calculator("1\n2\n3\n4"));
 
 module.exports = calculator;
