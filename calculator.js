ff29dd175684ac88bbab5870111e56216a16dde1
Sandeep <sandeepmodiwork@gmail.comm>
2024-10-09 21:24:05 +0530

    CAL-03-Sum for string calculation

--- calculator.js
+++ calculator.js
@@ -2,8 +2,22 @@ const calculator = (nums) => {
   if (!nums) {
     return 0;
   }
+
+  let total = 0;
+
+  const list = nums.split(",").map((el) => Math.round(el));
+
+  list.forEach((num) => {
+    total = total + num;
+  });
+
+  return total;
 };
 
-console.log(calculator());
+console.log(calculator(0));
+console.log(calculator("2,3,4,5"));
+console.log(calculator("2,3.3,4.1,5.2"));
+console.log(calculator("2,3,4.6,5.8"));
+console.log(calculator("2,3,4,5    "));
 
 module.exports = calculator;
