cea807cf350243dc5af617d3c1c60249fab6826a
Sandeep <sandeepmodiwork@gmail.comm>
2024-10-09 21:33:42 +0530

    CAL-06-Error for negative values

--- calculator.js
+++ calculator.js
@@ -19,25 +19,38 @@ const calculator = (nums) => {
     nums = numbers;
   }
 
-  let total = 0;
+  let negativeNumbers = [],
+    total = 0;
 
   const list = nums.split(",").map((el) => Math.round(el));
 
   list.forEach((num) => {
-    total = total + num;
+    if (num < 0) {
+      negativeNumbers.push(num);
+    } else {
+      total = total + num;
+    }
   });
+
+  if (negativeNumbers.length > 0) {
+    throw new Error(
+      `negative numbers not allowed ${negativeNumbers.join(", ")}`
+    );
+  }
 
   return total;
 };
 
-console.log(calculator(0));
-console.log(calculator("2,3,4,5"));
-console.log(calculator("2,3.3,4.1,5.2"));
-console.log(calculator("2,3,4.6,5.8"));
-console.log(calculator("2,3,4,5    "));
-console.log(calculator("1\n2\n3\n4"));
-console.log(calculator("//;\n1;2;3;4;5;"));
-console.log(calculator("//^\n9^8^7"));
-console.log(calculator("//^\n6^5^"));
+// console.log(calculator(0));
+// console.log(calculator("2,3,4,5"));
+// console.log(calculator("2,3.3,4.1,5.2"));
+// console.log(calculator("2,3,4.6,5.8"));
+// console.log(calculator("2,3,4,5    "));
+// console.log(calculator("1\n2\n3\n4"));
+// console.log(calculator("//;\n1;2;3;4;5;"));
+// console.log(calculator("//^\n9^8^7"));
+// console.log(calculator("//^\n6^5^"));
+// console.log(calculator("2,-3, -5"));
+// console.log(calculator("-5, 0"));
 
 module.exports = calculator;
